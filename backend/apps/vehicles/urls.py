a4f023e0ee5fbdca8cc7dd0664bd3817f252211d
Bekzod Malikov <malikovbekzodman@gmail.com>
2026-03-23 18:30:06 +0500

    brands and models getting api

--- backend/apps/vehicles/urls.py
+++ backend/apps/vehicles/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import VehicleCreateView, VehicleRetrieveUpdateDestroyView
+from .views import (
+    VehicleCreateView,
+    VehicleRetrieveUpdateDestroyView,
+    VehicleBrandListView,
+    VehicleModelListView,
+)
 
 urlpatterns = [
+    path('brands/', VehicleBrandListView.as_view(), name='vehicle-brand-list'),
+    path('models/', VehicleModelListView.as_view(), name='vehicle-model-list'),
     path('create/', VehicleCreateView.as_view(), name='vehicle-create'),
     path('<int:pk>/', VehicleRetrieveUpdateDestroyView.as_view(), name='vehicle-detail'),
 ]
